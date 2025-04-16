c92d1dc05526150e6a61e104c8ce2074ce39e916
Restorationmichael4 <156471511+Restorationmichael4@users.noreply.github.com>
2025-04-16 14:00:57 +0100

    Update index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { SocketProvider } from "./context/SocketContext.tsx";
 import './index.css' // Add this line
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
