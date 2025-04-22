3edcac63abcd290c219ee74e2d4f2ba803f07f50
Restorationmichael4 <156471511+Restorationmichael4@users.noreply.github.com>
2025-04-22 11:15:13 +0100

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -29,6 +29,9 @@ app.use(cors());
 const PORT = process.env.PORT || 5000;
 const SECRET_KEY = process.env.SECRET_KEY || "teenverse_secret";
 
+// Set FFmpeg path
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
 // Configure Cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
